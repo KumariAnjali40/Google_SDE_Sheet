b0ff692fad54d5934e3f99775191be8665066af3
kumari Anjali <142905766+KumariAnjali40@users.noreply.github.com>
2026-08-31 12:57:37 +0530

    Time: 15 ms (24.02%), Space: 46.4 MB (42.28%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map1 = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            map1.put(ch, map1.getOrDefault(ch,0)+1);
+        }
+
+        if (!map.equals(map1)) {
+            return false;
+        }
+
+        return true;
+    }
+}
